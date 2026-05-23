1f863087f6db469683a9e55cb02e4aa7d39d78b8
Lamia <lamiaalam@iut-dhaka.edu>
2026-05-23 14:33:26 +0600

    readme and utils updated

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Project Title: practice git
 Your Name: Lamia Alam
 
 What this project does: only for practicing git commands
+
+add func add, and subtract func subtracts
--- src/main.py
+++ src/main.py
@@ -5,3 +5,5 @@ name = "Lamia Alam Turna"
 print(f"Hello, {name}! Today is {datetime.date.today()}.")
 print(f"5 + 3 = {add(5, 3)}")
 print(f"5 - 3 = {subtract(5, 3)}")
+print(f"5 * 3 = {multiply(5, 3)}")
+print(f"5 / 3 = {divide(5, 3)}")
