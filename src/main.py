d432c9cd676645ca4fc4014dcec6b02aa6022243
Lamia <lamiaalam@iut-dhaka.edu>
2026-05-23 14:34:29 +0600

    age added

--- src/main.py
+++ src/main.py
@@ -7,3 +7,7 @@ print(f"5 + 3 = {add(5, 3)}")
 print(f"5 - 3 = {subtract(5, 3)}")
 print(f"5 * 3 = {multiply(5, 3)}")
 print(f"5 / 3 = {divide(5, 3)}")
+
+age = 30
+if age < 18:
+    print("You are a minor.")   
