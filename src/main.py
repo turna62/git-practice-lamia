063e7f089d7349c42fab14035748e019d9a9d6bc
Lamia <lamiaalam@iut-dhaka.edu>
2026-05-23 14:31:01 +0600

    Add basic calculator functions

--- src/main.py
+++ src/main.py
@@ -1,4 +1,7 @@
 import datetime
+from utils import add, subtract
 
 name = "Lamia Alam Turna"
 print(f"Hello, {name}! Today is {datetime.date.today()}.")
+print(f"5 + 3 = {add(5, 3)}")
+print(f"5 - 3 = {subtract(5, 3)}")
